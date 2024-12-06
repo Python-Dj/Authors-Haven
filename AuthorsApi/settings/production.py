b080aa9165e493987ca7959e0d22f19580da0575
Python-Dj <surya216gupta@gmail.com>
2024-12-06 13:27:29 +0530

    setup basic django project

---
+++ AuthorsApi/settings/production.py
@@ -0,0 +1,8 @@
+from .base import * #noqa
+from .base import env
+
+
+ADMINS = [("Suraj Gupta", "api.problemessage@email.com")]
+
+# TODO add domain names of the production server
+CSRF_TRUST_ORIGIN = [""]
